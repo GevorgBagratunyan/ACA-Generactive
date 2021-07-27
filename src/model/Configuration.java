3052da7d9675c15861d606ce16d2754e18905ffb
GevorgBagratunyan <g.bagratunyan@gmail.com>
2021-07-26 19:13:47 -0700

    project copy

---
+++ src/model/Configuration.java
@@ -0,0 +1,17 @@
+package model;
+
+import generactive.model.enums.Resolution;
+
+public class Configuration {
+
+    private Resolution resolution;
+
+    public Configuration(Resolution resolution) {
+        this.resolution = resolution;
+    }
+
+    public Resolution getResolution() {
+        return resolution;
+    }
+
+}
